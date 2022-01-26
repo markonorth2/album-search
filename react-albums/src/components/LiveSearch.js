00c9fd7417facd432c6393b29516323872bd2f12
Mark Chuang <markchuangth@gmail.com>
2022-01-25 20:50:40 -0500

    project start

---
+++ react-albums/src/components/LiveSearch.js
@@ -0,0 +1,22 @@
+import React, { Fragment, useState, useEffect } from "react";
+import axios from "axios";
+
+import SearchBar from "components/SearchBar";
+import Results from "components/Results";
+
+export default function LiveSearch(props) {
+  const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+
+  return (
+    <Fragment>
+      <header className="logo">
+        <img src="images/brand.png" alt="Brand" />
+      </header>
+      <main>
+        <SearchBar onSearch={term => setTerm(term)} />
+        <Results results={results} />
+      </main>
+    </Fragment>
+  );
+}
